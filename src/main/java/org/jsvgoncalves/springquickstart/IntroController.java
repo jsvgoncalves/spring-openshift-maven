1ec1976ce84c76d198d818e28413f9ac26efe14d
João Gonçalves <jsvgoncalves@gmail.com>
2016-09-16 23:39:40 +0200

    *wip*

--- src/main/java/org/jsvgoncalves/springquickstart/IntroController.java
+++ src/main/java/org/jsvgoncalves/springquickstart/IntroController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by jgoncalv on 9/13/16.
@@ -20,6 +22,18 @@ public class IntroController {
     public String intro(Model model, @PathVariable("name") String name) {
         model.addAttribute("name", name);
         return "intro/hello";
+    }
+
+    @RequestMapping(value = "/api/greeting", produces = "application/json")
+    @ResponseBody
+    public String greeting() {
+        return "[{\"author\": \"John\", \"text\": 1, \"timestamp\": 2}]";
+    }
+
+    @RequestMapping(value = "/api/greeting", produces = "application/json", method = RequestMethod.POST)
+    @ResponseBody
+    public String greetingPOST() {
+        return "[{\"author\": \"John\", \"text\": 1, \"timestamp\": 2},{\"author\": \"ASd\", \"text\": 1, \"timestamp\": 223}]";
     }
 
 }
